b7ef126759e9b3681f3ea30fbf6a4ef91c06e6de
gatlee-creator <gatlee@highpoint.edu>
2019-09-14 15:12:06 -0400

    Added data struct for player

--- playrugby.cpp
+++ playrugby.cpp
@@ -4,6 +4,18 @@
 
 using namespace std;
 
+//define the data structure 
+struct RugbyPlayer{
+    string playerName;
+    string position; 
+    int tries; 
+    int conversion;
+    float points;
+    int games;
+    int yellowCards; 
+};
+
+
 int main()
 {
 
