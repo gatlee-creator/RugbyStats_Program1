407742593c269d2b41fb34a99ceab16b82599c01
Graham Atlee <graham@Grahams-iMac.local>
2019-09-15 13:50:31 -0400

    const int for max players

--- playrugby.cpp
+++ playrugby.cpp
@@ -21,16 +21,18 @@ void readPlayerData(struct RugbyPlayer[], int, string);
 int main()
 {
      //10 rugby players
-    RugbyPlayer players[10]; 
+    const int maxPlayers = 10;
+    RugbyPlayer players[maxPlayers]; 
 
       //input filename
     const string nameOfInputFile = "rugby.txt"; 
 
 
-    readPlayerData(players, 10, nameOfInputFile); 
-    
-    // cout << players[3].playerName << players[3].position << endl;
-    // cout << players[8].playerName << players[8].position << endl;
+    readPlayerData(players, maxPlayers, nameOfInputFile); 
+
+    for(int i =0; i < 10; i++)
+        cout << players[i].playerName << players[i].position << players[i].tries << players[i].conversion << players[i].points << players[i].games << players[i].yellowCards <<  endl;
+
 
     return 0;
 }
@@ -49,7 +51,6 @@ void readPlayerData(struct RugbyPlayer players[], int max, string fileName){
                    >> players[i].games
                    >> players[i].yellowCards; 
     
-    cout << players[i].playerName << players[i].position << players[i].tries << players[i].conversion << players[i].points << players[i].games << players[i].yellowCards <<  endl;
     }
     inputFile.close(); 
 }
