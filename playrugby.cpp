38138eaa06c857633e85231fffb46019c2a74b9c
atlee19 <atleegraham14@gmail.com>
2019-09-22 16:00:48 -0400

    code fully documented

--- playrugby.cpp
+++ playrugby.cpp
@@ -1,3 +1,12 @@
+/*
+    Name: Graham Atlee
+    Course: csc1720-01
+    Location of Program: ~/csc1720/Programs/program1
+
+    This program is designed to take input data on rugby players and then 
+    give the user menu options to manipulate that data. 
+
+*/
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -16,6 +25,7 @@ struct RugbyPlayer{
     int yellowCards; 
 };
 
+//function prototypes
 void readPlayerData(struct RugbyPlayer[], int, string); 
 void printAllData(struct RugbyPlayer[], int);
 void printSingleData(int, struct RugbyPlayer[]); 
@@ -26,24 +36,28 @@ void swapData(struct RugbyPlayer[], int);
 void saveDataToFile(struct RugbyPlayer[], int, string); 
 void printSeperator(int);
 void printTableHeading(); 
+const int maxColWidth = 15; //global var used for table column width 
 
 int main()
 {
-     //10 rugby players
-    const int maxPlayers = 10;
-    int index; 
+      //declaring our data struct 
+    const int maxPlayers = 10; //we will only accept 10 players for now
     RugbyPlayer players[maxPlayers]; 
+
+      //declaring vars that will be used in menu 
+    int index;
     char menuChoice; 
     string searchName, fileName; 
 
     while(true){
+          //this is where the main menu begins 
         cout << "RUGBY TEAM MANAGER" << endl;
         printSeperator(30);
         cout << "[A] Upload player data" << endl;
         cout << "[B] Search for a player" << endl;
         cout << "[C] Display all player data" << endl; 
-        cout << "[D] Sort players by Tries" << endl; 
-        cout << "[E] Update a player Tries" << endl;
+        cout << "[D] Sort players by tries" << endl; 
+        cout << "[E] Update a player's tries" << endl;
         cout << "[F] Quit" << endl; 
         printSeperator(30);
         cout << "Please enter a character to select an option:" << endl;
@@ -52,23 +66,28 @@ int main()
         
         switch (menuChoice)
         {
-            case 'A':
+              //Upload player data 
+            case 'A':  
                 cout << "Enter name of file: " << endl;
-                //cin >> fileName;
-                readPlayerData(players, maxPlayers, "rugby.txt");
+                cin >> fileName;
+                readPlayerData(players, maxPlayers, fileName);
                 break;
-            
-            case 'B':
+
+              //Search for a player
+            case 'B': 
                 cout << "Enter name of player: " << endl;
                 cin >> searchName;
                 index = searchForPlayerData(searchName, players, maxPlayers); 
+                cout << endl;
                 printSingleData(index, players);
                 break; 
             
-            case 'C':
+             //Display all player data 
+            case 'C': 
                 printAllData(players, maxPlayers);
                 break; 
             
+              //Sort the player data by tries 
             case 'D':
                 cout << "[H] Highest to lowest " << endl;
                 cout << "[L] Lowest to Highest " << endl;
@@ -77,6 +96,7 @@ int main()
                 sortPlayers(players, maxPlayers, menuChoice);
                 break;
             
+              //Update a player's tries 
             case 'E':
                 cout << "Enter name of player: " << endl;
                 cin >> searchName; 
@@ -85,12 +105,13 @@ int main()
                 printSingleData(index, players);
                 break;
             
+              //Quit - exit the main menu/program
             case 'F':
                 cout << "Save data to a file? Enter Y to save or press any key to quit: " << endl;
                 cin >> menuChoice; 
 
                 if(menuChoice == 'Y'){
-                    //code to save to file
+                    //save the player data to file
                     cout << "Save file as: " << endl; 
                     cin >> fileName;
                     saveDataToFile(players, maxPlayers, fileName);
@@ -103,7 +124,7 @@ int main()
                 }
                 
 
-
+              //default idiot proofing 
             default:
                 cout << "Not a valid option..." << endl;
                 cout << endl; 
@@ -113,6 +134,7 @@ int main()
     return 0;
 }
 
+//function to save data to a new file 
 void saveDataToFile(struct RugbyPlayer dataSet[], int max, string fileName){
     ofstream outputFile; 
     outputFile.open(fileName);
@@ -128,12 +150,12 @@ void saveDataToFile(struct RugbyPlayer dataSet[], int max, string fileName){
 
     for(int i= 0; i < max; i++){
         outputFile << left << setw(12) 
-                << dataSet[i].playerName << setw(15) 
-                << dataSet[i].position << setw(15)
-                << dataSet[i].tries << setw(15)  
-                << dataSet[i].conversion << setw(15)
-                << dataSet[i].points << setw(15)
-                << dataSet[i].games << setw(15)
+                << dataSet[i].playerName << setw(maxColWidth) 
+                << dataSet[i].position << setw(maxColWidth)
+                << dataSet[i].tries << setw(maxColWidth)  
+                << dataSet[i].conversion << setw(maxColWidth)
+                << dataSet[i].points << setw(maxColWidth)
+                << dataSet[i].games << setw(maxColWidth)
                 << dataSet[i].yellowCards << endl;   
 
     }
@@ -142,6 +164,7 @@ void saveDataToFile(struct RugbyPlayer dataSet[], int max, string fileName){
     cout << endl;
 }
 
+//function that swaps all the players in part of resorting 
 void swapData(struct RugbyPlayer players[], int j){
     RugbyPlayer temp; 
       //store in temp struct
@@ -160,7 +183,7 @@ void swapData(struct RugbyPlayer players[], int j){
     players[j].points = players[j + 1].points;
     players[j].games = players[j + 1].games; 
     players[j].yellowCards = players[j + 1].yellowCards;
-      //transfer the data in temp back
+      //transfer the data in temp back to next index 
     players[j + 1].playerName = temp.playerName;
     players[j + 1].position =  temp.position;
     players[j + 1].tries = temp.tries;
@@ -171,8 +194,10 @@ void swapData(struct RugbyPlayer players[], int j){
 
 }
 
+//function to sort players by number of tries 
 void sortPlayers(struct RugbyPlayer players[], int max, char sortType){
     int i, j;
+      //Bubble sort from highest to lowest 
     if(sortType == 'H'){
         for(i=0; i < max - 1; i++){
             for(j=0; j < max - i - 1; j++){
@@ -182,6 +207,7 @@ void sortPlayers(struct RugbyPlayer players[], int max, char sortType){
             }
         }
     }
+      //Bubble sort from lowest to highest 
     else if(sortType == 'L'){
         for(i=0; i < max - 1; i++){
             for(j=0; j < max - i - 1; j++){
@@ -191,15 +217,17 @@ void sortPlayers(struct RugbyPlayer players[], int max, char sortType){
             }
         }
     }
+      //Idiot proofing options
     else{
         cout << "Invalid sorting option..." << endl;
         cout << endl;
     }
     
-
+    //after sort is comeplete we want to show the new data 
     printAllData(players, max);
 }
 
+//function to update a specific player's number of tries 
 void updatePlayerTries(int index, struct RugbyPlayer players[]){
     int newNumber;
     cout << "Enter new number of tries:" << endl;
@@ -210,25 +238,28 @@ void updatePlayerTries(int index, struct RugbyPlayer players[]){
 
 }
 
+//function to print data about a specific player 
 void printSingleData(int index, struct RugbyPlayer dataSet[] ){
     
     printTableHeading(); 
 
     cout << left << setw(12) 
-             << dataSet[index].playerName << setw(15) 
-             << dataSet[index].position << setw(15)
-             << dataSet[index].tries << setw(15)  
-             << dataSet[index].conversion << setw(15)
-             << dataSet[index].points << setw(15)
-             << dataSet[index].games << setw(15)
+             << dataSet[index].playerName << setw(maxColWidth) 
+             << dataSet[index].position << setw(maxColWidth)
+             << dataSet[index].tries << setw(maxColWidth)  
+             << dataSet[index].conversion << setw(maxColWidth)
+             << dataSet[index].points << setw(maxColWidth)
+             << dataSet[index].games << setw(maxColWidth)
              << dataSet[index].yellowCards << endl;   
 
-    cout << endl;     
+    cout << endl; //just some extra spacing (increase readability)    
 }
 
+//function that searchs for a specifc player by name 
 int searchForPlayerData(string searchName, struct RugbyPlayer dataSet[], int size){
     int indexFound; 
     bool isFound = false;
+     //using a simple selection search algo 
     for(int i = 0; i < size; i++){
         if(dataSet[i].playerName == searchName){
             indexFound = i;
@@ -238,27 +269,30 @@ int searchForPlayerData(string searchName, struct RugbyPlayer dataSet[], int siz
     }
     if(isFound == false)
         cout << "Player not found..." << endl;
+        cout << endl;
 
     return -1; 
 }
 
+//function to print all player data 
 void printAllData(struct RugbyPlayer players[], int max){
-
+    
     printTableHeading();   
     
     for(int i=0; i < max; i++){
         cout << left << setw(12) 
-             << players[i].playerName << setw(15) 
-             << players[i].position << setw(15)
-             << players[i].tries << setw(15)  
-             << players[i].conversion << setw(15)
-             << players[i].points << setw(15)
-             << players[i].games << setw(15)
+             << players[i].playerName << setw(maxColWidth) 
+             << players[i].position << setw(maxColWidth)
+             << players[i].tries << setw(maxColWidth)  
+             << players[i].conversion << setw(maxColWidth)
+             << players[i].points << setw(maxColWidth)
+             << players[i].games << setw(maxColWidth)
              << players[i].yellowCards << endl; 
     }
     cout << endl;
 }
 
+//function to read data from a file and store it in data struct 
 void readPlayerData(struct RugbyPlayer players[], int max, string fileName){
     ifstream inputFile; 
     inputFile.open(fileName);
@@ -286,18 +320,19 @@ void readPlayerData(struct RugbyPlayer players[], int max, string fileName){
     
 }
 
-//overload this 
+//function to print table heading to the console 
 void printTableHeading(){
     cout << left << setw(12)
-         << "Name" << setw(15) 
-         << "Position" << setw(15) 
-         << "Tries" << setw(15) 
-         << "Cov" << setw(15) 
-         << "Pts/Game" << setw(15) 
-         << "GS" << setw(15) 
+         << "Name" << setw(maxColWidth) 
+         << "Position" << setw(maxColWidth) 
+         << "Tries" << setw(maxColWidth) 
+         << "Cov" << setw(maxColWidth) 
+         << "Pts/Game" << setw(maxColWidth) 
+         << "GS" << setw(maxColWidth) 
          << "YC" << endl;
 }
 
+//function to print line seperators. Increase readability in the terminal 
 void printSeperator(int length){
     for(int i = 0; i < length; i++)
         cout << '=';
